2993cdb2912d58d3ea5bb49299692194c8114ade
nexus-428 <ronya-fischer@gmx.net>
2024-08-24 14:17:30 +0200

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -138,12 +138,12 @@ function getCookie(name) {
     var nameEQ = name + "=";
     var ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
+        var c = ca[i].trim();  // Verwende .trim(), um sicherzustellen, dass keine führenden Leerzeichen vorhanden sind
         if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
     return null;
 }
+
 
 function loadClarity() {
     (function(c,l,a,r,i,t,y){
